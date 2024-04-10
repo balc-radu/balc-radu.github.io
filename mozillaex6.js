4d3a8ad7f932205c89bf579ce1437b2575d61571
balc-radu <32979978+balc-radu@users.noreply.github.com>
2024-04-10 17:51:43 +0300

    Update mozillaex6.js

--- mozillaex6.js
+++ mozillaex6.js
@@ -1,7 +1,7 @@
 function checkCookieHasASpecificValue() {
   if (document.cookie.split(";").some((item) => item.includes("favorite_task=collect Data"))) {
     const output = document.getElementById("a-specific-value-of-the-cookie");
-    output.textContent = '> The cookie "reader" has a value of "1"';
+    output.textContent = '> The cookie "favorite_task" has a value of "collect Data"';
   }
 }
 
